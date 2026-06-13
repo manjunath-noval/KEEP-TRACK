f2e5823197020422c23404952407324c4fbf4d0b
manjunath-nova <manjunath.noval@gmail.com>
2026-06-13 11:33:56 +0530

    Configure Render deployment

--- config/db.js
+++ config/db.js
@@ -5,12 +5,13 @@ const connectDB = async () => {
     try{
 
         await mongoose.connect(
-            "mongodb://ProtrackAdmin:Manju%40861%23@ac-0qhcaa2-shard-00-00.7sfjdh5.mongodb.net:27017,ac-0qhcaa2-shard-00-01.7sfjdh5.mongodb.net:27017,ac-0qhcaa2-shard-00-02.7sfjdh5.mongodb.net:27017/protrack?ssl=true&replicaSet=atlas-137lc7-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+            process.env.MONGO_URI
         );
 
         console.log("MongoDB Connected");
 
     }
+
     catch(error){
 
         console.log(error);
